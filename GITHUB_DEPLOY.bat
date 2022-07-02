35467ff0dbd7bdcbfa2ccef39550999dc386adf9
Alessandro-Crespi <alessandro.crespi.2002@gmail.com>
2022-07-02 10:02:48 +0200

    v1.2.4

--- GITHUB_DEPLOY.bat
+++ GITHUB_DEPLOY.bat
@@ -1,9 +1,22 @@
 @ECHO OFF
 XCOPY C:\Users\ASUS\Desktop\WORK\FINANZ\nova_investor\build . /E /H /C /I /Y
+echo ---------------------
+echo ADDING CHANGES...
+echo ---------------------
 git add --all
-set /p msg="Enter commit message"
+
+echo ---------------------
+set /p msg="Enter commit message:   "
+echo ---------------------
+echo COMMITTING...
+echo ---------------------
 git commit -m "%msg%"
+echo ---------------------
+echo PUSHING...
+echo ---------------------
 git push -u origin main
-echo All done!
+echo ---------------------
+echo ALL DONE. Press a key to continue...
+echo ---------------------
 PAUSE > nul
 @ECHO ON
