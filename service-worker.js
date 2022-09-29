ffa3460b05858f32c2399ac4d587d38809ce7e81
Alessandro-Crespi <alessandro.crespi.2002@gmail.com>
2022-09-29 11:58:21 +0200

    upload

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ubiquitous-octo-disco/precache-manifest.3cb10df3bbe28bd54f596ea5116ee4c5.js"
+  "./precache-manifest.b2f035432c0f4a8373ba988babdf0360.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ubiquitous-octo-disco/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
